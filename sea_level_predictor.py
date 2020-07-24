ad55ef9af17ef692b5de11b34f51550f9cbc5309
João Pedro Peters Barbosa <joao.peters@engenharia.ufjf.br>
2020-07-24 01:59:58 +0000

    Added 'description= ' to pyproject.toml file, under [tool.poetry] section

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,23 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
+import sys
 
 def draw_plot():
-    # Read data from file
+  # Read data from file
+  sealvl = pd.read_csv('epa-sea-level.csv')
+  sys.exit()
+  # Create scatter plot
 
 
-    # Create scatter plot
+  # Create first line of best fit
 
 
-    # Create first line of best fit
+  # Create second line of best fit
 
 
-    # Create second line of best fit
+  # Add labels and title
 
-
-    # Add labels and title
-
-    
-    # Save plot and return data for testing (DO NOT MODIFY)
-    plt.savefig('sea_level_plot.png')
-    return plt.gca()
+  
+  # Save plot and return data for testing (DO NOT MODIFY)
+  plt.savefig('sea_level_plot.png')
+  return plt.gca()
